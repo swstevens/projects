f4c8db414adcf55c2baab54e88c4ef97f98de0c0
swstevens <sstevensw@gmail.com>
2023-12-10 18:39:08 -0800

    fix fork to show rendering projeect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React, { Component } from "react";
 import ReactGA from "react-ga";
-import $ from "jquery";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ProjectsPage from './Components/projects'
-import MainPage from './Components/mainpage'
-
-const HOME_ROUTE = '/'
-const PROJECTS_ROUTE = '/projects'
 
 class App extends Component {
   constructor(props) {
@@ -21,33 +15,9 @@ class App extends Component {
     ReactGA.pageview(window.location.pathname);
   }
 
-  getResumeData() {
-    $.ajax({
-      url: "./resumeData.json",
-      dataType: "json",
-      cache: false,
-      success: function(data) {
-        this.setState({ resumeData: data });
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
-
-  componentDidMount() {
-    this.getResumeData();
-  }
-
   render() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route exact path={HOME_ROUTE} element={<MainPage data={this.state.resumeData} />}/>
-                <Route exact path={PROJECTS_ROUTE} element={<ProjectsPage data={this.state.resumeData}/>}/>
-            </Routes>
-        </BrowserRouter>
+      <ProjectsPage />
     );
   }
 }
