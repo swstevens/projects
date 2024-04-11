e526626c9f05f0410f1f1bf069dc0f50a24b8b09
swstevens <sstevensw@gmail.com>
2024-04-10 18:07:44 -0700

    add dropdown for further work

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { AsciiRenderer, OrbitControls } from "@react-three/drei";
+import ReactSelect from 'react-select';
 
 function Box () {
   return (
@@ -14,7 +15,7 @@ function Box () {
 function TorusKnot(){
   const ref = React.useRef()
   useFrame((state, delta) => (ref.current.rotation.x = ref.current.rotation.y += delta / 2))
- 
+
   return (
     <instancedMesh args={[null, null, 10]} ref={ref}>
       <torusKnotGeometry args={[7, 3, 100, 16]}></torusKnotGeometry>
@@ -25,17 +26,28 @@ function TorusKnot(){
 
 
 export const ProjectsPage = () => {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
   return (
-    <div style={{height:"100vh"}}>
-      <Canvas alpha={true} camera={{position: [0, 25, 0] }}>
-          <OrbitControls />
-          <color attach="background" args={['black']} />
-          <AsciiRenderer fgColor="white" bgColor="transparent"/>
-          <ambientLight intensity={0.5} />
-          <directionalLight position={[10, 15, 10]} intensity={1}/>
-          <TorusKnot />
+    <>
+    <div styles="z-index: 9999">
+    <ReactSelect options={options} placeholder = {"Select an Option"}/>
+    </div>
+    <div style={{height:"100vh" , width:"100vw", position: "absolute"}} styles="z-index: 0">
+
+      <Canvas alpha={true} camera={{position: [0, 25, 0] }} styles="z-index: 0" >
+        <OrbitControls />
+        <color attach="background" args={['black']} />
+        <AsciiRenderer fgColor="white" bgColor="transparent"/>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 15, 10]} intensity={1}/>
+        <TorusKnot />
       </Canvas>
     </div>
+    </>
   )
 }
 
