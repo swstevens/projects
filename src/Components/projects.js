11a26861848090ba6498f15a60034df50474a2ce
swstevens <sstevensw@gmail.com>
2024-04-14 21:12:36 -0700

    implement outline

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -14,7 +14,17 @@ function Box () {
   )
 }
 
-function TorusKnot(){
+// const solidify = (mesh) => {
+//   const geometry = mesh.geometry
+//   const material = new THREE.shaderMaterial({
+//     vertexShader,
+//     fragmentShader,
+//   })
+
+//   const outline = new THREE.Mesh(geometry, material)
+// }
+
+function TorusOutline(){
   const ref = React.useRef()
   const data = useMemo(
     () => ({
@@ -27,7 +37,8 @@ function TorusKnot(){
         Shininess: { value: 200.0 }
       },
       fragmentShader,
-      vertexShader
+      vertexShader,
+      side: THREE.BackSide
     }),
     []
   )
@@ -35,49 +46,32 @@ function TorusKnot(){
 
   return (
     <instancedMesh args={[null, null, 10]} ref={ref}>
-      <torusKnotGeometry args={[7, 3, 100, 16]}></torusKnotGeometry>
-      <meshPhongMaterial color="tomato" />
+      <torusKnotGeometry args={[7, 3, 100, 17]}></torusKnotGeometry>
+      <meshPhongMaterial color="tomato"/>
       <shaderMaterial attach="material" {...data} />
     </instancedMesh>
   );
 };
 
+function TorusKnot(){
+  const ref = React.useRef()
+  useFrame((state, delta) => (ref.current.rotation.x = ref.current.rotation.y += delta / 2))
+
+  return (
+    <instancedMesh args={[null, null, 10]} ref={ref}>
+      <torusKnotGeometry args={[7, 3, 100, 16]}></torusKnotGeometry>
+      <meshPhongMaterial color="black" />
+    </instancedMesh>
+  );
+};
+
 const fragmentShader = `
-  varying vec3 Normal;
-  varying vec3 Position;
-
-  uniform vec3 Ka;
-  uniform vec3 Kd;
-  uniform vec3 Ks;
-  uniform vec4 LightPosition;
-  uniform vec3 LightIntensity;
-  uniform float Shininess;
-
-  vec3 phong() {
-    vec3 n = normalize(Normal);
-    vec3 s = normalize(vec3(LightPosition) - Position);
-    vec3 v = normalize(vec3(-Position));
-    vec3 r = reflect(-s, n);
-
-    vec3 ambient = Ka;
-    vec3 diffuse = Kd * max(dot(s, n), 0.0);
-    vec3 specular = Ks * pow(max(dot(r, v), 0.0), Shininess);
-
-    return LightIntensity * (ambient + diffuse + specular);
-  }
-
   void main() {
-    vec3 blue = vec3(0.0, 0.0, 1.0);
-    gl_FragColor = vec4(blue*phong(), 1.0);
+    gl_FragColor = vec4(1,0,0,1);
 }`
 
 const vertexShader = `
-  varying vec3 Normal;
-  varying vec3 Position;
-
   void main() {
-    Normal = normalize(normalMatrix * normal);
-    Position = vec3(modelViewMatrix * vec4(position, 1.0));
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
   }
 `
@@ -106,6 +100,7 @@ export const ProjectsPage = () => {
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 15, 10]} intensity={1}/>
         <TorusKnot />
+        <TorusOutline/>
       </Canvas>
     </div>
     </>
